7eb3952c0b8287d001251bdc23865e429ed906cf
ㅤㅤㅤ⏤͟͟͞͞•𓆩𖤍𓆪ꪾ 𝐃𝜩‌𝐕𝐈𝐋⛧‌ٖٖٖٖٖٖٜٖٖٖٖٖٖٜٖㅤ [•ᴧғᴋ•] ㅤㅤ <105618799+OMKARa011@users.noreply.github.com>
2024-08-30 15:10:28 +0530

    Update start.py
    Signed-off-by: ㅤㅤㅤ⏤͟͟͞͞•𓆩𖤍𓆪ꪾ 𝐃𝜩‌𝐕𝐈𝐋⛧‌ٖٖٖٖٖٖٜٖٖٖٖٖٖٜٖㅤ [•ᴧғᴋ•] ㅤㅤ <105618799+OMKARa011@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/aIexa_updates"),
+            InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/+gXUQQ6g3w74wZTg1"),
         ],
     ]
     return buttons
@@ -26,11 +26,11 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/AIEXA_SUPPORT"),
+            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/+gXUQQ6g3w74wZTg1"),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/aIexa_updates"),
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/FenixAva"),
         ],
     ]
     return buttons
